e8ee2cb0983c33ba59db6482be6ce6cfcdb06670
Alexandr_Demin <schurick.don@gmail.com>
2024-01-21 19:24:14 +0300

    Added API qase

--- src/test/java/tests/withdrawalRequests/ClickRequest.java
+++ src/test/java/tests/withdrawalRequests/ClickRequest.java
@@ -1,16 +1,12 @@
 package tests.withdrawalRequests;
-
 import io.qase.api.annotation.QaseId;
-import io.qase.api.annotation.QaseTitle;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
-import pages.base.BasePage;
 import tests.base.BaseTest;
 
 public class ClickRequest extends BaseTest {
 
     @Test
-    @QaseTitle("https://app.qase.io/case/TB-6")
+    @QaseId(6)
     public void CliclRequest(){
         basePage.open("http://45.8.248.89:5500/");
 
